790bae99978ce67e4dd377be97b0d40498cf2cd5
md-ratul <mdratulanjum@gmail.com>
2023-10-18 13:28:58 -0700

    five

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
 
+require("dotenv").config();
 const mongoose = require("mongoose");
+const url = process.env.DB_URL || "mongodb://127.0.0.1:27017/studentsDb";
 
 exports.dbConnect = async () =>{
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/studentsDb");
+        await mongoose.connect(url);
         console.log("database is connected");
     }catch(error){
         console.log(error);
